78792ee9155a1bcd5430809a6a43113780b27579
Clarke Brunsdon <clarke@freerunningtechnologies.com>
2016-07-06 03:42:53 -0700

    Add more board printing functions
    Gotta love dat ascii

--- lib/slytherin/board_printer.rb
+++ lib/slytherin/board_printer.rb
@@ -7,10 +7,18 @@ module Slytherin
       [
         "-" * (board.width + 2),
         board.height.times.map { |height|
-          '|'  + (' ' * board.width ) + '|'
+          '|'  + board.width.times.map { |width|
+            space(board, width, height) }.join('') + '|'
         },
         "-" * (board.width + 2)
       ].flatten.join("\n")
+    end
+
+    private
+    def space(board, width, height)
+      return 'f' if board.food.select { |food| food == [width, height] }.any?
+      return 'h' if board.snakes.select { |snake| snake.head == [width, height] }.any?
+      ' '
     end
   end
 end
--- test/test_board_printer.rb
+++ test/test_board_printer.rb
@@ -1,5 +1,6 @@
 require "minitest/autorun"
 require 'slytherin/board_printer'
+require 'slytherin/snake'
 require 'slytherin/board'
 
 class TestBoardPrinter < Minitest::Test
@@ -8,7 +9,7 @@ class TestBoardPrinter < Minitest::Test
     Slytherin::Board.new 3, 3, snakes, food
   end
 
-  def test_print_3x3
+  def test_print_empty_3x3
     board =
       "-----\n" +
       "|   |\n" +
@@ -16,6 +17,29 @@ class TestBoardPrinter < Minitest::Test
       "|   |\n" +
       "-----"
     assert_equal Slytherin::BoardPrinter.new.print(three_by_three_board), board
+  end
+
+  def test_print_food_3x3
+    board =
+      "-----\n" +
+      "|   |\n" +
+      "| f |\n" +
+      "|   |\n" +
+      "-----"
+
+    assert_equal Slytherin::BoardPrinter.new.print(three_by_three_board([], [[1,1]])), board
+  end
+
+  def test_print_snake_head
+    board =
+      "-----\n" +
+      "|   |\n" +
+      "| h |\n" +
+      "|   |\n" +
+      "-----"
+
+    snake = Slytherin::Snake.new('snakey', [[1,1]])
+    assert_equal Slytherin::BoardPrinter.new.print(three_by_three_board([snake], [])), board
   end
 
 end
